586906d21ce50c8e9a914e9700a26464a2722c9d
박지훈 <pjh@ziumks.com>
2024-06-03 10:23:37 +0900

    useReducer 인풋 상태 관리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-
-import Counter from "./components/Counter";
+import Info from "./components/Info";
 function App() {
-  return <Counter />;
+  return <Info />;
 }
 
 export default App;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,32 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value, /// switch문 대신 배열 사용, 배열의 index값을 키로 사용
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, dispatch] = useReducer(reducer, { name: "", nickname: "" });
 
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
