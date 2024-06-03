cf796508bc8e815bbbe21af8a982bc1d9e888de6
박지훈 <pjh@ziumks.com>
2024-06-03 09:53:36 +0900

    useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+import Info from "./components/Info";
 function App() {
-  return <Counter></Counter>;
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
